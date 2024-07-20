13ba5695a18b9a0b574ef358a7c74a8bf902a2e6
Sohail Khan <sohailkhan9098@gmail.com>
2024-07-20 23:37:56 +0200

    main

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"log"
+
+	"github.com/Sohail-9098/vehicle-data-collector/internal/config"
+	"github.com/Sohail-9098/vehicle-data-collector/internal/mqtt"
+)
+
 func main() {
+	mqttConfig, err := config.NewMQTTConfig()
+	if err != nil {
+		log.Fatalf("failed to read config file: %v", err)
+	}
+	client := mqtt.NewClient(mqttConfig.MQTT.Broker, mqttConfig.MQTT.ClientID, mqttConfig.MQTT.Username, mqttConfig.MQTT.Password)
+	client.Connect()
+	client.Disconnect()
 }

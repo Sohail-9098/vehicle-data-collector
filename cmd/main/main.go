0af5e7ebb6d52dcfbfc4d4178f2e14855141e829
Sohail Khan <sohailkhan9098@gmail.com>
2024-07-21 16:21:10 +0200

    refactor

--- cmd/main/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "github.com/Sohail-9098/vehicle-data-collector/internal/telemetry"
-
-func main() {
-	data := telemetry.NewTelemetryData()
-	data.FetchAndProcessTelemetryData()
-}
